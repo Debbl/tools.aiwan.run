360769b4076968e3ac2adea877aeace8be112455
Brendan Dash <me@aiwan.run>
2025-04-07 23:28:20 +0800

    fix: i18n

--- src/app/(main)/pinyin/layout.tsx
+++ src/app/(main)/pinyin/layout.tsx
@@ -1,11 +1,10 @@
-import { t } from "@lingui/core/macro";
 import type { Metadata } from "next/types";
 import type { ReactNode } from "react";
 
 export const metadata: Metadata = {
-  title: t`Pinyin`,
-  description: t`Convert chinese to pinyin`,
-  icons: ["/pinyin/icon.svg"],
+  title: "Pinyin",
+  description: "Convert chinese to pinyin",
+  icons: "/pinyin/icon.svg",
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
