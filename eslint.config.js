a8d06c2f673241e64ae30330a81f64b3fc393402
Brendan Dash <me@aiwan.run>
2025-05-07 23:10:15 +0800

    chore: update deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,18 @@
 // @ts-check
 import { defineConfig } from "@debbl/eslint-config";
 
-export default defineConfig({
-  typescript: true,
-  react: {
-    next: true,
-    compiler: true,
+export default defineConfig(
+  {
+    typescript: true,
+    react: {
+      next: true,
+      compiler: true,
+    },
+    tailwindcss: true,
   },
-  tailwindcss: true,
-  customConfig: {
+  {
     rules: {
       "react-dom/no-unknown-property": "off",
     },
   },
-});
+);
